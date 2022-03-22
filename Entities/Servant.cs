6f385d8b885b16dc234e5eed35ced030227f334b
Charlls Ruiz <ruizlorrenz@gmail.com>
2022-03-21 19:46:05 -0600

    initial creation; added entities and helper class; set connectionString;

---
+++ Entities/Servant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FGOSummonSimulator.Entities {
+    public class Servant {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public char LimitedYN { get; set; }
+        public char StoryLockedYN { get; set; }
+        public int Rarity { get; set; }
+    }
+}
