c90fea304d1aecdad70c7225f466d6b5581cd3ee
Charlls Ruiz <ruizlorrenz@gmail.com>
2022-04-04 19:17:33 -0600

    implemented CraftEssenceRepository

---
+++ Utility/Repositories/CraftEssenceRepository.cs
@@ -0,0 +1,19 @@
+﻿using Dapper;
+using FGOSummonSimulator.Entities;
+using FGOSummonSimulator.Utility.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FGOSummonSimulator.Utility.Repositories {
+    public class CraftEssenceRepository : ICraftEssenceRepository {
+        public List<CraftEssence> GetCEByRarityAndLimitedYN(int rarity, char limitedYN) {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("FGOSSDB"))) {
+                return connection.Query<CraftEssence>("dbo.uspGetCraftEssencesByRarityAndLimitedYN @Rarity, @LimitedYN", new { rarity = rarity, limitedYN = limitedYN }).ToList();
+            }
+        }
+    }
+}
