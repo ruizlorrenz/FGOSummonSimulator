f75d85bf4cc03036dec48963b1bdf68084bb3476
Charlls Ruiz <ruizlorrenz@gmail.com>
2022-04-04 19:11:46 -0600

    added repository interfaces

---
+++ Utility/Interfaces/Repositories/IServantRepository.cs
@@ -0,0 +1,14 @@
+﻿using FGOSummonSimulator.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FGOSummonSimulator.Utility.Interfaces.Repositories {
+    public interface IServantRepository {
+        List<Servant> GetServantsByName(string name);
+        List<Servant> GetServantsByRarity(int rarity);
+        List<Servant> GetServantsByRarityAndLimitedYN(int rarity, char limitedYN);
+    }
+}
